3c27fc1e0916795f2c87da280c70388dd9391ca4
EnxGitHub <65719051+EnxGitHub@users.noreply.github.com>
2020-06-04 21:48:47 +0300

    Update TexttoSpeech.py

--- TexttoSpeech.py
+++ TexttoSpeech.py
@@ -3,12 +3,6 @@ import os
 from playsound import playsound
 while True:
     language = "en"
-    if language.lower().strip() == "tr" or language.lower().strip() == "en": 
-        break
-    else:
-        print("Please enter a valid language!")
-        continue
-
     mytext = str(input("\nPlease enter a text:\t"))
     output = gTTS(text=mytext,lang=language,slow=False)
     output.save("output.mp3")
