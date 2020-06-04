37f01fbfeb3d153720f820d8252b7b6d73a9cb3c
EnxGitHub <65719051+EnxGitHub@users.noreply.github.com>
2020-06-04 18:24:52 +0300

    Add files via upload

---
+++ TexttoSpeech.py
@@ -0,0 +1,18 @@
+from gtts  import gTTS
+import os
+import time
+from playsound import playsound
+while True:
+    language = "en"
+    if language.lower().strip() == "tr" or language.lower().strip() == "en": 
+        break
+    else:
+        print("Please enter a valid language!")
+        continue
+
+while True:
+    mytext = str(input("\nPlease enter a text:\t"))
+    output = gTTS(text=mytext,lang=language,slow=False)
+    output.save("output.mp3")
+    playsound("output.mp3")
+    os.remove("output.mp3")
